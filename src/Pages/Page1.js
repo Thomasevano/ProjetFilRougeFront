9410d9ec28e545ac809e82b055d578f7fbda8670
LimongiVirgil <limongi.virgil@gmail.com>
2019-12-08 18:16:30 +0100

    merge dev and fix bug

--- src/Pages/Page1.js
+++ src/Pages/Page1.js
@@ -6,10 +6,10 @@ function Page1() {
   return (
     <div>
       <section className="visualText">
-        <TextImage position="right"/>
+        <TextImage position="right" img="https://static.cnews.fr/sites/default/files/styles/image_640_360/public/capture_decran_2019-10-22_a_11.32.54_5daecce35d7b4_0.png?itok=Zc1cUYqC"/>
       </section>
       <section className="visualText">
-        <TextImage position="left"/>
+        <TextImage position="left" img="https://blog.nameshield.com/fr/wp-content/uploads/sites/3/2018/02/JO-PyeongChang-2018-cyberattaque-Blog-Nameshield.jpg"/>
       </section>
       <EndPart text="A platea sit mi senectus nec egestas eget. Tincidunt massa velit vitae volutpat viverra nunc. Leo ipsum egestas quis lacus accumsan ?"/>
     </div>
