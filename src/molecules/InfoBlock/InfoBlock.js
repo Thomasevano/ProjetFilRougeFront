79debe68e47e4bc98f16ec42bb24d3116512ce65
Thomas Evano <thomas-evano@hotmail.fr>
2019-12-20 15:22:19 +0100

    - Adding Montserrat font - Fix Montserrat text - Fix InfoBlock SCSS

--- src/molecules/InfoBlock/InfoBlock.js
+++ src/molecules/InfoBlock/InfoBlock.js
@@ -3,7 +3,7 @@ import React from 'react';
 function InfoBlock() {
   return(
     <div className="info-block">
-      <h1 className="text info-title">1m</h1>
+      <h1 className="text info-title">1 m</h1>
       <h2 className="text info-subtitle">tons</h2>
       <p className="text info-description">The city of Paris produces about 1 million tons of waste per year, and only 19% of this volume is recycled.</p>
     </div>
