267cc9d3bad0bb63c33a5fe5489a3be4b7e58ad3
LimongiVirgil <limongi.virgil@gmail.com>
2019-12-20 15:10:18 +0100

    add scroll horizontal

--- src/dataViz/timeLine.js
+++ src/dataViz/timeLine.js
@@ -10,24 +10,31 @@ class TimeLine extends Component {
   }
     
   drawChart() {
-    const data = [12, 5, 6, 6, 9, 10];
+    const data = [12, 5, 6, 6, 9, 10, 12, 5, 6, 6, 9, 10, 12, 5, 6, 6, 9, 10, 12, 5, 6, 6, 9, 10];
     
     const svg = d3.select(".timeLine")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .style("background-color", "#F3F8F9");
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .style("background-color", "#F3F8F9");
 
-                  
     svg.selectAll("rect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * 70)
+      .attr("x", (d, i) => i * 100)
       .attr("y", (d, i) => h - 10 * d)
       .attr("width", 65)
       .attr("height", (d, i) => d * 10)
       .attr("fill", "green")
+
+    svg.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+      .text((d) => d)
+      .attr("x", (d, i) => i * 100)
+      .attr("y", (d, i) => h - (10 * d) - 3)
   }
         
   render(){
