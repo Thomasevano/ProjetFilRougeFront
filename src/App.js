4b7399b7592ad0bf5c2afa48316dc85d1c5e3cb5
LimongiVirgil <limongi.virgil@gmail.com>
2020-01-16 11:37:50 +0100

    add window size checker to display

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './App.scss';
 import Routes from './Pages/Route';
 
+const WinSize = () => {
+  if (window.innerWidth < 568) {
+    return (
+      <div className='wrongSize'>
+        <div className="wrongSizeContent">
+          <p>Votre résolution d'écran ne permet pas un affichage optimal de notre expérience. Veuillez tourner votre appareil en format paysage.</p>
+        </div>
+      </div>
+    )
+  }
+  return false
+}
+
 function App() {
   return (
     <div className="App">
       <Routes/>
+      <WinSize />
     </div>
   );
 }
