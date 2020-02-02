f7e83f2df9f26ed16305397634d3ed27ad1b0559
LimongiVirgil <limongi.virgil@gmail.com>
2020-02-02 13:34:12 +0100

    fix timeLine

--- src/dataViz/treeMap.js
+++ src/dataViz/treeMap.js
@@ -12,82 +12,79 @@ class TreeMap extends Component {
 
     const data = {"children": this.props.data}
 
-    console.log(data)
+    // set the dimensions and margins of the graph
+    const sizeWindow = window.innerWidth - 115;
 
-  // set the dimensions and margins of the graph
-  var margin = {
+    var margin = {
       top: 10,
       right: 10,
       bottom: 10,
       left: 10
     },
-    width = 445 - margin.left - margin.right,
-    height = 445 - margin.top - margin.bottom;
 
-  // append the svg object to the body of the page
-  var svg = d3.select(".treeMap")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+    width = sizeWindow - margin.left - margin.right,
+    height = 650 - margin.top - margin.bottom;
+    
 
-  // read json data
+    // append the svg object to the body of the page
+    var svg = d3.select(".treeMap")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Give the data to this cluster layout:
-  var root = d3.hierarchy(data).sum((d) =>
-    d.nbTrilibs + d.nbTrimobiles
-  ) // Here the size of each leave is given in the 'value' field in input data
+    // Give the data to this cluster layout:
+    var root = d3.hierarchy(data).sum((d) =>
+      d.nbTrilibs + d.nbTrimobiles
+    ) // Here the size of each leave is given in the 'value' field in input data
 
-  // Then d3.treemap computes the position of each element of the hierarchy
-  d3.treemap()
-    .size([width, height])
-    .padding(2)
-    (root)
+    // Then d3.treemap computes the position of each element of the hierarchy
+    d3.treemap()
+      .size([width + 50, height + 50])
+      .padding(20)
+      (root)
 
-  // use this information to add rectangles:
-  svg
-    .selectAll("rect")
-    .data(root.leaves())
-    .enter()
-    .append("rect")
-    .attr('x', function (d) {
-      return d.x0;
-    })
-    .attr('y', function (d) {
-      return d.y0;
-    })
-    .attr('width', function (d) {
-      return d.x1 - d.x0;
-    })
-    .attr('height', function (d) {
-      return d.y1 - d.y0;
-    })
-    .style("stroke", "black")
-    .style("fill", "slateblue")
+    // use this information to add rectangles:
+    svg.selectAll("rect")
+      .data(root.leaves())
+      .enter()
+      .append("rect")
+      .attr('x', function (d) {
+        return d.x0;
+      })
+      .attr('y', function (d) {
+        return d.y0;
+      })
+      .attr('width', function (d) {
+        return (d.x1 - d.x0);
+      })
+      .attr('height', function (d) {
+        return (d.y1 - d.y0);
+      })
+      .style("stroke", "black")
+      .style("fill", "slateblue")
 
-  // and to add the text labels
-  svg
-    .selectAll("text")
-    .data(root.leaves())
-    .enter()
-    .append("text")
-    .attr("x", function (d) {
-      return d.x0 + 5
-    }) // +10 to adjust position (more right)
-    .attr("y", function (d) {
-      return d.y0 + 20
-    }) // +20 to adjust position (lower)
-    .text(function (d) {
-      return d.data.name
-    })
-    .attr("font-size", "15px")
-    .attr("fill", "white")
+    // and to add the text labels
+    svg.selectAll("text")
+      .data(root.leaves())
+      .enter()
+      .append("text")
+      .attr("x", function (d) {
+        return d.x0 + 5
+      }) // +10 to adjust position (more right)
+      .attr("y", function (d) {
+        return d.y0 + 20
+      }) // +20 to adjust position (lower)
+      .text(function (d) {
+        return d.data.name
+      })
+      .attr("font-size", "15px")
+      .attr("fill", "white")
   }
 
   render(){
-    return <div className="treeMap" id={"#" + this.props.id}></div>
+    return <div className="treeMap"></div>
   }
 }
 
