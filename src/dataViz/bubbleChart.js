ac904a8decf671a8c903ad65c4c14f74f26f895b
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-04 22:00:44 +0100

    -Fix BubbleChart, il s'affiche bien maintenant - Add names and tons on bubble

--- src/dataViz/bubbleChart.js
+++ src/dataViz/bubbleChart.js
@@ -5,31 +5,59 @@ function BubbleChart(wasteData) {
   const waste = wasteData.wasteData
   const svgRef = useRef();
 
-  const width = 100 + '%';
-  const height = 1700;
+  const width = 1241;
+  const height = 700;
+  let svg = createSVG();
 
   useEffect(() => {
-    const svg = d3.select(svgRef.current)
-      .attr('width', width)
-      .attr('height', height)
-    svg
-      .selectAll('circle')
-      .data(waste)
-      .join('circle')
-      .attr('r', waste => waste.tons / 3)
-      .attr('cx', 1000 )
-      .attr('cy', waste => waste.tons + 10)
-      .attr('fill-opacity', 0.7)
-      // .attr('fill', '#E2E2E2');
-      .attr('fill', 'navy');
-    // svg
-    //   .append('text')
-    //   .data(waste)
-    //     .text(waste => waste.name)
+    let hierachalData = makeHierarchy(waste)
+    let packLayout = pack([width - 5, height - 5])
+    const root = packLayout(hierachalData);
+
+    const leaf = svg
+      .selectAll("g")
+      .data(root.leaves())
+      .join("g")
+      .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+
+    leaf.append("circle")
+        .attr("r", d => d.r)
+        .attr("fill-opacity", 0.7)
+        .attr("fill", "blue");
+  
+    leaf.append("text")
+        .data(waste)
+      .join("tspan")
+        .attr("x", 0)
+        .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+        .text(d => d.name)
+  
+    leaf.append("text")
+        .data(waste)
+        .text(d => d.tons + ' tons');
+      
   }, [waste])
 
+  function createSVG() {
+    return d3.select(svgRef.current)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("style", "background-color: #F3F8F9")
+  }
+
+  function pack(size) {
+    return d3.pack()
+    .size(size)
+    .padding(3)
+  }
+
+  function makeHierarchy(data) {
+    return d3.hierarchy({ children: data}).sum(d => d.tons)
+  }
+
   return (
-    <svg className="bubbleChart" ref={svgRef}></svg>
+      <div className="wasteAmount" ref={svgRef}></div>
   )
 }
 
