32d6abf081378e3b6fb4ad016614a55c22ba1b1c
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-07 18:24:41 +0100

    -Add style and other Data to dataviz

--- src/dataViz/bubbleChart.js
+++ src/dataViz/bubbleChart.js
@@ -1,12 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-function BubbleChart(wasteData) {
-  const waste = wasteData.wasteData
+function BubbleChart(wasteOneDay, wasteTwoWeeks, wasteOlympicsOneDay, wasteOlympicsTwoWeeks) {
+  // const [waste, setWaste] = useState([wasteParisOneDay.wasteParisOneDay])
+  let waste = wasteOneDay.wasteOneDay
+  const wastePTW = wasteOneDay.wasteTwoWeeks
+  const wastePOOD = wasteOneDay.wasteOlympicsOneDay
+  const wastePOTW = wasteOneDay.wasteOlympicsTwoWeeks
   const svgRef = useRef();
 
   const width = 1241;
-  const height = 700;
+  const height = 600;
+  let div = createDiv();
   let svg = createSVG();
 
   useEffect(() => {
@@ -18,32 +23,75 @@ function BubbleChart(wasteData) {
       .selectAll("g")
       .data(root.leaves())
       .join("g")
-      .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`);
+      // .attr("transform", d => `translate(${d.y - 200},${d.x + 100})`);
+      .attr("transform", d => `translate(${d.y + 200},${d.x - 330})`);
 
     leaf.append("circle")
         .attr("r", d => d.r)
-        .attr("fill-opacity", 0.7)
-        .attr("fill", "blue");
+        .attr("fill", "#C4C4C4");
   
     leaf.append("text")
         .data(waste)
-      .join("tspan")
-        .attr("x", 0)
-        .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+        .attr('class', 'data-text data-name')
+        .attr("x", - 4 + "%")
+        // .attr("y", (d, i, nodes) => `${i - nodes.length / 2 + 0.8}em`)
+        .attr("y", - 30)
         .text(d => d.name)
   
     leaf.append("text")
         .data(waste)
+        .attr('class', 'data-text data-tons')
+        .attr('x', - 3 + '%')
         .text(d => d.tons + ' tons');
-      
-  }, [waste])
+    
+    svg.append('svg')
+    .attr('class', 'circle-opacity')
+    .attr("width", 200)
+    .attr("height", 250)
+    .append('circle')
+    .attr('cx', '0')
+    .attr('cy', '140')
+    .attr('r', '120')
+    .attr('stroke-width', '0')
+    .attr('fill', '#FB8070')
 
+    svg.append('svg')
+    .attr('class', 'circle')
+    .attr("width", 200)
+    .attr("height", 200)
+    .append('circle')
+    .attr('cx', '0')
+    .attr('cy', '90')
+    .attr('r', '65')
+    .attr('stroke-width', '0')
+    .attr('fill', '#F3F8F9')
+
+    div.append('div')
+    .attr('class', 'totalTons')
+    .style('width', "257px")
+    .style('height', "101px")
+    .append('p')
+    .text('TOTAL')
+    .append('p')
+    .text(waste.reduce((accumulator, d)=> accumulator + d.tons ,0) + ' tons')
+  }, [waste])
+  
   function createSVG() {
+  return div
+    .append("svg")
+    .attr("class", "wasteAmount")
+    .attr("width", width)
+    .attr("height", height)
+  }
+
+  function createDiv() {
     return d3.select(svgRef.current)
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("style", "background-color: #F3F8F9")
+      // .attr("width", width)
+      // .attr("height", height)
+  }
+
+  function makeHierarchy(data) {
+    return d3.hierarchy({ children: data}).sum(d => d.tons)
   }
 
   function pack(size) {
@@ -52,12 +100,12 @@ function BubbleChart(wasteData) {
     .padding(3)
   }
 
-  function makeHierarchy(data) {
-    return d3.hierarchy({ children: data}).sum(d => d.tons)
-  }
-
   return (
-      <div className="wasteAmount" ref={svgRef}></div>
+    <div>
+      <button onClick= {() => waste = waste.map(tons => tons * 13.4)}>2 weeks in paris</button>
+      <div className="wasteAmountBlock" ref={svgRef}></div>
+    </div>
+    
   )
 }
 
--- src/molecules/BubbleChartBlock/BubbleChartBlock.js
+++ src/molecules/BubbleChartBlock/BubbleChartBlock.js
@@ -2,18 +2,47 @@ import React, { useState, useEffect} from 'react'
 import BubbleChart from '../../dataViz/bubbleChart';
 
 function BubbleChartBlock() {
-  const [waste, setWaste] = useState([])
+  const [wasteOneDay, setWasteOneDay] = useState([])
+  const [wasteTwoWeeks, setWasteTwoWeeks] = useState([])
+  const [wasteOlympicsOneDay, setWasteOlympicsOneDay] = useState([])
+  const [wasteOlympicsTwoWeeks, setWasteOlympicsTwoWeeks] = useState([])
 
-  useEffect(() => {
+  function getWasteOneDay() {
     fetch('http://127.0.0.1:8000/records-waste')
     .then(response => response.json())
-    .then(result => setWaste(result))
+    .then(result => setWasteOneDay(result));
+  }
+
+  function getWasteTwoWeeks() {
+    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/14/false')
+    .then(response => response.json())
+    .then(result => setWasteTwoWeeks(result))
+  }
+
+  function getWasteOlympicsOneDay() {
+    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/1/true')
+    .then(response => response.json())
+    .then(result => setWasteOlympicsOneDay(result))
+  }
+
+  function getWasteOlympicsTwoWeeks() {
+    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/14/true')
+    .then(response => response.json())
+    .then(result => setWasteOlympicsTwoWeeks(result))
+  }
+
+  useEffect(() => {
+    getWasteOneDay()
+    getWasteTwoWeeks()
+    getWasteOlympicsOneDay()
+    getWasteOlympicsTwoWeeks()
   }, [])
   
   return(
     <div className="bubbleChartBlock">
       <h1>Amounts of waste that could break records</h1>
-      <BubbleChart wasteData={waste} />
+      <BubbleChart wasteOneDay={wasteOneDay} wasteTwoWeeks={wasteTwoWeeks}
+       wasteOlympicsOneDay={wasteOlympicsOneDay} wasteOlympicsTwoWeeks={wasteOlympicsTwoWeeks} />
     </div>
   )
 }
