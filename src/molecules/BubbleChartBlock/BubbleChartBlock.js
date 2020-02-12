ccf9e0fd5ff837cc87544c2f0aa4597b37cd4b0e
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-12 14:06:17 +0100

    - Refactor BubbleChart - Add Button to update data - Add style to BubbleChart

--- src/molecules/BubbleChartBlock/BubbleChartBlock.js
+++ src/molecules/BubbleChartBlock/BubbleChartBlock.js
@@ -2,47 +2,20 @@ import React, { useState, useEffect} from 'react'
 import BubbleChart from '../../dataViz/bubbleChart';
 
 function BubbleChartBlock() {
-  const [wasteOneDay, setWasteOneDay] = useState([])
-  const [wasteTwoWeeks, setWasteTwoWeeks] = useState([])
-  const [wasteOlympicsOneDay, setWasteOlympicsOneDay] = useState([])
-  const [wasteOlympicsTwoWeeks, setWasteOlympicsTwoWeeks] = useState([])
-
-  function getWasteOneDay() {
-    fetch('http://127.0.0.1:8000/records-waste')
-    .then(response => response.json())
-    .then(result => setWasteOneDay(result));
-  }
-
-  function getWasteTwoWeeks() {
-    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/14/false')
-    .then(response => response.json())
-    .then(result => setWasteTwoWeeks(result))
-  }
-
-  function getWasteOlympicsOneDay() {
-    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/1/true')
-    .then(response => response.json())
-    .then(result => setWasteOlympicsOneDay(result))
-  }
-
-  function getWasteOlympicsTwoWeeks() {
-    fetch('http://127.0.0.1:8000/records-waste-multiplicateur/14/true')
-    .then(response => response.json())
-    .then(result => setWasteOlympicsTwoWeeks(result))
-  }
+  const [waste, setWaste] = useState([])
+  const [nbDays, setNbDays] = useState(1)
+  const [isOlympic, setIsOlympic] = useState(false)
 
   useEffect(() => {
-    getWasteOneDay()
-    getWasteTwoWeeks()
-    getWasteOlympicsOneDay()
-    getWasteOlympicsTwoWeeks()
-  }, [])
+    fetch(`http://127.0.0.1:8000/records-waste-multiplicateur/${nbDays}/${isOlympic}`)
+    .then(response => response.json())
+    .then(result => setWaste(result))
+  }, [nbDays,isOlympic])
   
   return(
     <div className="bubbleChartBlock">
       <h1>Amounts of waste that could break records</h1>
-      <BubbleChart wasteOneDay={wasteOneDay} wasteTwoWeeks={wasteTwoWeeks}
-       wasteOlympicsOneDay={wasteOlympicsOneDay} wasteOlympicsTwoWeeks={wasteOlympicsTwoWeeks} />
+      <BubbleChart waste={waste} setWaste={setWaste} nbDays={nbDays} setNbDays={setNbDays} isOlympic={isOlympic} setIsOlympic={setIsOlympic}/>
     </div>
   )
 }
