054254ab02b5b8857cdd2f883a053fc9961d101d
LimongiVirgil <limongi.virgil@gmail.com>
2020-02-14 09:17:38 +0100

    fix scroll navigation page

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './atoms/ScrollToTop/index.js'
 
 ReactDOM.render((
   <Router>
-    <App />
+    <ScrollToTop/>
+      <App />
   </Router>
 ), document.getElementById('root'));
 
