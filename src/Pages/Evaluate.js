32e33e4089f31f6befafb10cff73410367eabfe4
Virgil Limongi <virgil.limongi@danris.fr>
2020-02-19 18:19:06 +0100

    add barChart to page evaluate

--- src/Pages/Evaluate.js
+++ src/Pages/Evaluate.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Navbar from '../molecules/Navbar/Navbar';
 import IntroBlock from '../molecules/IntroBlock/IntroBlock';
-// import ChartBarSet from '../molecules/ChartBar/index'
-
+import ChartBarSet from '../molecules/ChartBar/index'
+ 
 function Evaluate() {
   return (
     <div>
       <Navbar/>
       <section className="intro">
-        <IntroBlock img="./data/intro-background-eiffelTower.jpg" alt="eiffel tower view" title="Participate" stepNumber="step 2" infoTitle="" infoSubtitle="HOW TO USE THE MONDRIAN GRID" infoDescription="Click on the different locations below to see how you can limit your impact"/>
+        <IntroBlock img="./data/intro-background-eiffelTower.jpg" alt="eiffel tower view" title="Evaluate" stepNumber="step 3" infoTitle="" infoSubtitle="HOW TO USE THE MONDRIAN GRID" infoDescription="Click on the different locations below to see how you can limit your impact"/>
       </section>
       <section className="dataVizChartBar">
-        {/* <ChartBarSet /> */}
+        <ChartBarSet />
       </section>
     </div>
   )
