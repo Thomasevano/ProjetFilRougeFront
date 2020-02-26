b5c1658907e3b9898974297a156b9143bb936fa2
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-26 14:37:53 +0100

    -Fix size of bubble in BubbleChart -Add is_recyclabe and different color in bubble -Fix center text in bubble

--- src/dataViz/bubbleChart.js
+++ src/dataViz/bubbleChart.js
@@ -3,23 +3,28 @@ import * as d3 from 'd3';
 import Filter from '../atoms/FilterDataviz/Filter';
 
 const BubbleChart = ({waste}) => {
-  const width = 600;
-  const height = 600;
-  let sizeWindow = 600;
-  
+  let width = 1250;
+  let height = 950;
+  if (window.innerWidth <= 375) {
+    width = 375;
+  }
+  else if (window.innerWidth <= 768){
+    width = 600;
+  }
+  let color
   const svgRef = useRef();
   const div = d3.select(svgRef.current);
   const svg = createSVG();
   let hierachalData = makeHierarchy(waste)
-  let packLayout = pack([sizeWindow - 5 , sizeWindow - 5])
+  let packLayout = pack([height - 5 , width - 5])
   const root = packLayout(hierachalData);
 
   function createSVG() {
   return div
     .append("svg")
     .attr("class", "wasteAmount")
-    .attr("width", sizeWindow)
-    .attr("height", sizeWindow)
+    .attr("width", width)
+    .attr("height", height)
   }
 
   function makeHierarchy(data) {
@@ -40,26 +45,29 @@ const BubbleChart = ({waste}) => {
       .enter()
       .append('g')
       .attr('class', 'node')
-      .attr("transform", d =>  `translate(${d.y},${d.x - 50})`)
+      .attr("transform", d =>  `translate(${d.y},${d.x - 115})`)
       .append('g')
       .attr('class', 'graph');
 
     node
       .append('circle')
       .attr('class', 'circle')
-      .attr('r', d => d.r / 2)
-      .attr("fill", "#C4C4C4")
+      .attr('r', d => d.r / 1.8)
+      .attr("fill", d => d.data.is_recyclabe ? color = "#E5E5E5" : color = "#C4C4C4")
 
     node
       .append('text')
       .attr('class', 'data-text data-name')
-      .attr("y", - 24)
+      .attr("y", - 12)
       .text(d => d.data.name)
 
     node
       .append('text')
       .attr('class', 'data-text data-tons')
+      .attr("y", + 12)
       .text(d => d.data.tons + ' tons');
+
+      console.log(d => d)
   }
 
   let totalTons = 
@@ -96,16 +104,16 @@ const BubbleChart = ({waste}) => {
         .innerHTML = (`<p>TOTAL</p>
         <p>${data.reduce((accumulator, d) => Math.round(accumulator + d.tons * 100)/100 ,0) + ' tons'}</p>`)
       if (days === 14 && olympic === false) {
-        circle.attr('r', d => d.r / 1.5)
-      }
-      else if (days === 14 && olympic === true) {
-        circle.attr('r', d => d.r)
-      }
-      else if (days === 1 && olympic === true) {
         circle.attr('r', d => d.r / 1.3)
       }
+      else if (days === 14 && olympic === true) {
+        circle.attr('r', d => d.r )
+      }
+      else if (days === 1 && olympic === true) {
+        circle.attr('r', d => d.r / 1.5)
+      }
       else if (days === 1 && olympic === false) {
-        circle.attr('r', d => d.r / 2)
+        circle.attr('r', d => d.r / 1.8)
       }
     }
   }
