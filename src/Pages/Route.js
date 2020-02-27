4c4ebbfe55bbb55ea2dcd1f4e8b69b6365a5544a
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-27 23:20:28 +0100

    -Add CSS Transition between pages

--- src/Pages/Route.js
+++ src/Pages/Route.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Home from './Home';
 import Sensitization from './Sensitization';
 import Participate from './Participate';
-import Evaluate from './Evaluate'
+import Evaluate from './Evaluate';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-function Routes() {
+function Routes({location}) {
   
   return (
-    <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/sensitization" exact component={Sensitization}/>
-      <Route path="/participate" exact component={Participate}/>
-      <Route path="/evaluate" exact component={Evaluate}/>
-    </Switch>
+    <TransitionGroup>
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={'fade'}
+        >
+        <div className="route-section">
+          <Switch location={location}>
+            <Route path="/" exact component={Home}/>
+            <Route path="/sensitization" exact component={Sensitization}/>
+            <Route path="/participate" exact component={Participate}/>
+            <Route path="/evaluate" exact component={Evaluate}/>
+          </Switch>
+        </div>
+        </CSSTransition>
+    </TransitionGroup>
   )
 }
 
-export default Routes
+export default withRouter(Routes)
