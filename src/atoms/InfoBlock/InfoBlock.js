50d896a2acaa056f3489791c7db7ee200fe92161
Thomas Evano <thomasevano@MacBook-Pro-de-Thomas.local>
2020-02-27 20:10:19 +0100

    -fix some errors -Add animation on home logo button & social media button

--- src/atoms/InfoBlock/InfoBlock.js
+++ src/atoms/InfoBlock/InfoBlock.js
@@ -5,7 +5,7 @@ function InfoBlock({infoTitle, infoSubtitle, infoDescription, imgTop}) {
   
   return(
     <div className="info-block">
-      {imgTop && <img src={imgTop} alt="image top"/>}
+      {imgTop && <img src={imgTop} alt="top background"/>}
       <h1 className="text info-title">{infoTitle}</h1>
       <h2 className="text info-subtitle">{infoSubtitle}</h2>
       <p className="text info-description" dangerouslySetInnerHTML={{__html:infoDescription}}></p>
